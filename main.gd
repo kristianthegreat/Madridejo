ec5eddc7802343068f9892a1cae82392179a6997
kristianthegreat <madridejo.kristian070603@gmail.com>
2026-05-01 16:53:28 +0800

    Game Uploaded

---
+++ main.gd
@@ -0,0 +1,53 @@
+extends Node
+
+@export var mob_scene: PackedScene 
+
+var score = 0
+
+func _ready():
+	pass
+	
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$StartTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$GameOver.play()
+	
+func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+	
+func _on_score_timer_timeout():
+	score += 1
+	$HUD.update_score(score)
+		
+func _on_start_timer_timeout():
+	# Start gameplay timers after delay
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+	
+func _on_mob_timer_timeout():
+	
+	var mob = mob_scene.instantiate()
+	
+	var mob_spawn = $MobPath/MobSpawn
+	mob_spawn.progress_ratio = randf()
+	
+	var direction = mob_spawn.rotation + PI / 2
+	mob.position = mob_spawn.position
+	
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	var velocity = Vector2(randf_range(150.0, 450.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	add_child(mob)
